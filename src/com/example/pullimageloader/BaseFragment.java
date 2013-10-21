9d3477d8ea112dd12c7533dd272b2bb3b5f629b7
Yu Lu <yulu8798@gmail.com>
2013-10-21 22:40:54 +0800

    add all

---
+++ src/com/example/pullimageloader/BaseFragment.java
@@ -0,0 +1,191 @@
+package com.example.pullimageloader;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+import android.annotation.SuppressLint;
+import android.app.Fragment;
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.handmark.pulltorefresh.library.PullToRefreshBase;
+import com.handmark.pulltorefresh.library.PullToRefreshBase.Mode;
+import com.handmark.pulltorefresh.library.PullToRefreshBase.OnRefreshListener;
+import com.handmark.pulltorefresh.library.PullToRefreshListView;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.assist.ImageLoadingListener;
+import com.nostra13.universalimageloader.core.assist.SimpleImageLoadingListener;
+import com.nostra13.universalimageloader.core.display.FadeInBitmapDisplayer;
+import com.nostra13.universalimageloader.core.display.RoundedBitmapDisplayer;
+
+
+@SuppressLint("NewApi")
+public class BaseFragment extends Fragment{
+	
+	protected PullToRefreshListView mPullRefreshListView;
+	protected ImageLoader imageLoader = ImageLoader.getInstance();
+	DisplayImageOptions options;
+	private List<String> list;
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+	      Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.pull_list,container, false);
+		
+		//image loader options
+		options = new DisplayImageOptions.Builder()
+		.showImageOnLoading(R.drawable.ic_stub)
+		.showImageForEmptyUri(R.drawable.ic_empty)
+		.showImageOnFail(R.drawable.ic_error)
+		.cacheInMemory(true)
+		.cacheOnDisc(true)
+		.displayer(new RoundedBitmapDisplayer(20))
+		.build();
+		
+		list = new ArrayList<String>();
+		for(int i = 0 ; i < imageUrls.length; i++){
+			list.add(imageUrls[i]);
+		}
+		
+		
+		//get the pull list id in xml
+		mPullRefreshListView = (PullToRefreshListView) view.findViewById(R.id.pull_refresh_list);
+		
+		//set to pull from end mode
+		mPullRefreshListView.setMode(Mode.PULL_FROM_END);
+		
+		//set refresh listener
+		mPullRefreshListView.setOnRefreshListener(new OnRefreshListener<ListView>() {
+			@Override
+			public void onRefresh(PullToRefreshBase<ListView> refreshView) {
+
+				//TODO: load the data in background thread
+				new GetDataTask().execute();
+			}
+		});
+		
+		// Need to use the Actual ListView when registering for Context Menu
+		ListView actualListView = mPullRefreshListView.getRefreshableView();
+		
+		registerForContextMenu(actualListView);
+
+		actualListView.setAdapter(new ItemAdapter());
+		
+		return view;
+	}
+	
+	private class GetDataTask extends AsyncTask<Void, Void, String[]> {
+
+		@Override
+		protected String[] doInBackground(Void... params) {
+			// Simulates a background job.
+			try {
+				Thread.sleep(20);
+			} catch (InterruptedException e) {
+			}
+			return imageUrls2;
+		}
+
+		@Override
+		protected void onPostExecute(String[] result) {
+
+			// Call onRefreshComplete when the list has been refreshed.
+			for(int i = 0; i < result.length; i++){
+				list.add(result[i]);
+			}
+			mPullRefreshListView.onRefreshComplete();
+
+			super.onPostExecute(result);
+		}
+	}
+	
+
+	class ItemAdapter extends BaseAdapter {
+
+		private ImageLoadingListener animateFirstListener = new AnimateFirstDisplayListener();
+
+		private class ViewHolder {
+			public TextView text;
+			public ImageView image;
+		}
+
+		@Override
+		public int getCount() {
+			return list.size();
+		}
+
+		@Override
+		public Object getItem(int position) {
+			return position;
+		}
+
+		@Override
+		public long getItemId(int position) {
+			return position;
+		}
+
+		@Override
+		public View getView(final int position, View convertView, ViewGroup parent) {
+			View view = convertView;
+			final ViewHolder holder;
+			if (convertView == null) {
+				view = getActivity().getLayoutInflater().inflate(R.layout.item_list_image, parent, false);
+				holder = new ViewHolder();
+				holder.text = (TextView) view.findViewById(R.id.text);
+				holder.image = (ImageView) view.findViewById(R.id.image);
+				view.setTag(holder);
+			} else {
+				holder = (ViewHolder) view.getTag();
+			}
+
+			holder.text.setText("Item " + (position + 1));
+
+			imageLoader.displayImage(list.get(position), holder.image, options, animateFirstListener);
+
+			return view;
+		}
+	}
+
+	private static class AnimateFirstDisplayListener extends SimpleImageLoadingListener {
+
+		static final List<String> displayedImages = Collections.synchronizedList(new LinkedList<String>());
+
+		@Override
+		public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
+			if (loadedImage != null) {
+				ImageView imageView = (ImageView) view;
+				boolean firstDisplay = !displayedImages.contains(imageUri);
+				if (firstDisplay) {
+					FadeInBitmapDisplayer.animate(imageView, 500);
+					displayedImages.add(imageUri);
+				}
+			}
+		}
+	}
+	
+	public static String[] imageUrls = new String[] {
+		// Heavy images
+		"https://lh6.googleusercontent.com/-55osAWw3x0Q/URquUtcFr5I/AAAAAAAAAbs/rWlj1RUKrYI/s1024/A%252520Photographer.jpg",
+		"https://lh4.googleusercontent.com/--dq8niRp7W4/URquVgmXvgI/AAAAAAAAAbs/-gnuLQfNnBA/s1024/A%252520Song%252520of%252520Ice%252520and%252520Fire.jpg",
+		"https://lh5.googleusercontent.com/-7qZeDtRKFKc/URquWZT1gOI/AAAAAAAAAbs/hqWgteyNXsg/s1024/Another%252520Rockaway%252520Sunset.jpg",
+	};
+	
+	public static String[] imageUrls2 = new String[] {
+		"https://lh5.googleusercontent.com/-CG62QiPpWXg/URqu4ia4vRI/AAAAAAAAAbs/0YOdqLAlcAc/s1024/Napali%252520Coast.jpg",
+		"https://lh6.googleusercontent.com/-wdGrP5PMmJQ/URqu5PZvn7I/AAAAAAAAAbs/m0abEcdPXe4/s1024/One%252520Wheel.jpg",
+		"https://lh6.googleusercontent.com/-6WS5DoCGuOA/URqu5qx1UgI/AAAAAAAAAbs/giMw2ixPvrY/s1024/Open%252520Sky.jpg",
+		"https://lh6.googleusercontent.com/-u8EHKj8G8GQ/URqu55sM6yI/AAAAAAAAAbs/lIXX_GlTdmI/s1024/Orange%252520Sunset.jpg",
+	};
+	
+}
